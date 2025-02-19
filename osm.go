eab1a6553f8cb3c91a1cd1540a226b55e75c62d4
pchchv <ipchchv@gmail.com>
2025-02-19 11:20:27 +0300

    Add findType function to retrieve type from JSON data

--- osm.go
+++ osm.go
@@ -1,5 +1,21 @@
 package osm
 
+import "fmt"
+
 type typeS struct {
 	Type string `json:"type"`
+}
+
+func findType(index int, data []byte) (string, error) {
+	ts := typeS{}
+	if err := unmarshalJSON(data, &ts); err != nil {
+		// should not happened due to previous decoding succeeded
+		return "", err
+	}
+
+	if ts.Type == "" {
+		return "", fmt.Errorf("could not find type in element index %d", index)
+	}
+
+	return ts.Type, nil
 }
