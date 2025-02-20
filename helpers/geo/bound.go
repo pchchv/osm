4ca101a865595a34201827738db24926333d5766
pchchv <ipchchv@gmail.com>
2025-02-20 09:36:05 +0300

    Add Equal and GeoJSONType methods to Bound type for comparison and GeoJSON representation

--- helpers/geo/bound.go
+++ helpers/geo/bound.go
@@ -5,3 +5,13 @@ type Bound struct {
 	Min Point
 	Max Point
 }
+
+// Equal returns if two bounds are equal.
+func (b Bound) Equal(c Bound) bool {
+	return b.Min == c.Min && b.Max == c.Max
+}
+
+// GeoJSONType returns the GeoJSON type for the object.
+func (b Bound) GeoJSONType() string {
+	return "Polygon"
+}
