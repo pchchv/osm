38d88326d765dbd4ed792104a7ef312131a8247d
pchchv <ipchchv@gmail.com>
2025-02-20 09:38:32 +0300

    Add methods to Bound type for retrieving box edges: Top, Bottom, Right, and Left

--- helpers/geo/bound.go
+++ helpers/geo/bound.go
@@ -1,5 +1,7 @@
 package geo
 
+var emptyBound = Bound{Min: Point{1, 1}, Max: Point{-1, -1}}
+
 // Bound represents a closed box or rectangle.
 type Bound struct {
 	Min Point
@@ -24,4 +26,24 @@ func (b Bound) Dimensions() int {
 // Bound returns the the same bound.
 func (b Bound) Bound() Bound {
 	return b
+}
+
+// Top returns the top of the bound.
+func (b Bound) Top() float64 {
+	return b.Max[1]
+}
+
+// Bottom returns the bottom of the bound.
+func (b Bound) Bottom() float64 {
+	return b.Min[1]
+}
+
+// Right returns the right of the bound.
+func (b Bound) Right() float64 {
+	return b.Max[0]
+}
+
+// Left returns the left of the bound.
+func (b Bound) Left() float64 {
+	return b.Min[0]
 }
