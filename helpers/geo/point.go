2983d4cbec714645b1c09b61cf2c3ec8a29c730c
pchchv <ipchchv@gmail.com>
2025-02-20 09:29:53 +0300

    Implement Pointer interface methods for Point type in geo package

--- helpers/geo/point.go
+++ helpers/geo/point.go
@@ -1,4 +1,21 @@
 package geo
 
+var _ Pointer = Point{}
+
 // Point is a Lon/Lat 2d point.
 type Point [2]float64
+
+// Point returns itself so it implements the Pointer interface.
+func (p Point) Point() Point {
+	return p
+}
+
+// Lon returns the horizontal, longitude coordinate of the point.
+func (p Point) Lon() float64 {
+	return p[0]
+}
+
+// Equal checks if the point represents the same point or vector.
+func (p Point) Equal(point Point) bool {
+	return p[0] == point[0] && p[1] == point[1]
+}
