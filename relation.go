04745c5c5cd4b363f6d037407e2f9a7ada6d6519
pchchv <ipchchv@gmail.com>
2025-03-22 07:40:41 +0300

    Implement ObjectID and ElementID methods for RelationID type

--- relation.go
+++ relation.go
@@ -1,10 +1,20 @@
 package osm
 
 // RelationID is the primary key of a relation.
-// A relation is uniquely identifiable by the id + version.
+// Relation is uniquely identifiable by the id + version.
 type RelationID int64
 
 // FeatureID is a helper returning the feature id for this relation id.
 func (id RelationID) FeatureID() FeatureID {
 	return FeatureID(relationMask | id<<versionBits)
+}
+
+// ObjectID is a helper returning the object id for this relation id.
+func (id RelationID) ObjectID(v int) ObjectID {
+	return ObjectID(id.ElementID(v))
+}
+
+// ElementID is a helper to convert the id to an element id.
+func (id RelationID) ElementID(v int) ElementID {
+	return id.FeatureID().ElementID(v)
 }
