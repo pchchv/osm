b399418e63066b6af9e842add410e59422f754f3
pchchv <ipchchv@gmail.com>
2025-03-22 07:42:59 +0300

    Implement ObjectID, FeatureID, and ElementID methods for WayID type

--- way.go
+++ way.go
@@ -3,3 +3,18 @@ package osm
 // WayID is the primary key of a way.
 // Way is uniquely identifiable by the id + version.
 type WayID int64
+
+// ObjectID is a helper returning the object id for this way id.
+func (id WayID) ObjectID(v int) ObjectID {
+	return ObjectID(id.ElementID(v))
+}
+
+// FeatureID is a helper returning the feature id for this way id.
+func (id WayID) FeatureID() FeatureID {
+	return FeatureID(wayMask | (id << versionBits))
+}
+
+// ElementID is a helper to convert the id to an element id.
+func (id WayID) ElementID(v int) ElementID {
+	return id.FeatureID().ElementID(v)
+}
