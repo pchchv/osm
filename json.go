aa933320463f91a4dd7d302d022db7ed890cc5e8
pchchv <ipchchv@gmail.com>
2025-03-22 07:37:27 +0300

    Add custom JSON marshaling for Note, Changeset, and User types

--- json.go
+++ json.go
@@ -67,6 +67,45 @@ func (x xmlNameJSONTypeRel) UnmarshalJSON(data []byte) error {
 	return nil
 }
 
+// xmlNameJSONTypeNote is kind of a hack
+// to encode the proper json
+// object type attribute for this struct type.
+type xmlNameJSONTypeNote xml.Name
+
+func (x xmlNameJSONTypeNote) MarshalJSON() ([]byte, error) {
+	return []byte(`"note"`), nil
+}
+
+func (x xmlNameJSONTypeNote) UnmarshalJSON(data []byte) error {
+	return nil
+}
+
+// xmlNameJSONTypeCS is kind of a hack
+// to encode the proper json
+// object type attribute for this struct type.
+type xmlNameJSONTypeCS xml.Name
+
+func (x xmlNameJSONTypeCS) MarshalJSON() ([]byte, error) {
+	return []byte(`"changeset"`), nil
+}
+
+func (x xmlNameJSONTypeCS) UnmarshalJSON(data []byte) error {
+	return nil
+}
+
+// xmlNameJSONTypeUser is kind of a hack
+// to encode the proper json
+// object type attribute for this struct type.
+type xmlNameJSONTypeUser xml.Name
+
+func (x xmlNameJSONTypeUser) MarshalJSON() ([]byte, error) {
+	return []byte(`"user"`), nil
+}
+
+func (x xmlNameJSONTypeUser) UnmarshalJSON(data []byte) error {
+	return nil
+}
+
 func marshalJSON(v interface{}) ([]byte, error) {
 	if CustomJSONMarshaler == nil {
 		return json.Marshal(v)
