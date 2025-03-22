a1e0a0f24c27260698ddf8c072854194c2cd2211
pchchv <ipchchv@gmail.com>
2025-03-22 07:18:42 +0300

    Add NewBoundsFromTile function to create Bounds from map tile index

--- bounds.go
+++ bounds.go
@@ -1,11 +1,35 @@
 package osm
 
+import (
+	"errors"
+
+	"github.com/pchchv/geo/maptile"
+)
+
 // Bounds are the bounds of osm data as defined in the xml file.
 type Bounds struct {
 	MinLat float64 `xml:"minlat,attr"`
 	MaxLat float64 `xml:"maxlat,attr"`
 	MinLon float64 `xml:"minlon,attr"`
 	MaxLon float64 `xml:"maxlon,attr"`
+}
+
+// NewBoundsFromTile creates a bound given an online map tile index.
+func NewBoundsFromTile(t maptile.Tile) (*Bounds, error) {
+	maxIndex := uint32(1 << t.Z)
+	if t.X >= maxIndex {
+		return nil, errors.New("osm: x index out of range for this zoom")
+	} else if t.Y >= maxIndex {
+		return nil, errors.New("osm: y index out of range for this zoom")
+	}
+
+	b := t.Bound()
+	return &Bounds{
+		MinLat: b.Min.Lat(),
+		MaxLat: b.Max.Lat(),
+		MinLon: b.Min.Lon(),
+		MaxLon: b.Max.Lon(),
+	}, nil
 }
 
 // ObjectID returns the bounds type but with 0 id.
