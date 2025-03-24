dd8a5be01d185d5eb877404448852f866f17e937
pchchv <ipchchv@gmail.com>
2025-03-24 09:01:50 +0300

    Add ChangesetDiscussion type and method MarshalXML

--- changeset.go
+++ changeset.go
@@ -1,6 +1,9 @@
 package osm
 
-import "time"
+import (
+	"encoding/xml"
+	"time"
+)
 
 // ChangesetID is the primary key for an osm changeset.
 type ChangesetID int64
@@ -16,4 +19,28 @@ type ChangesetComment struct {
 	UserID    UserID    `xml:"uid,attr" json:"uid"`
 	Timestamp time.Time `xml:"date,attr" json:"date"`
 	Text      string    `xml:"text" json:"text"`
+}
+
+// ChangesetDiscussion is a conversation about a changeset.
+type ChangesetDiscussion struct {
+	Comments []*ChangesetComment `xml:"comment" json:"comments"`
+}
+
+// MarshalXML implements the xml.Marshaller method to exclude this
+// whole element if the comments are empty.
+func (csd ChangesetDiscussion) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
+	if len(csd.Comments) == 0 {
+		return nil
+	}
+
+	if err = e.EncodeToken(start); err != nil {
+		return
+	}
+
+	t := xml.StartElement{Name: xml.Name{Local: "comment"}}
+	if err = e.EncodeElement(csd.Comments, t); err != nil {
+		return
+	}
+
+	return e.EncodeToken(start.End())
 }
