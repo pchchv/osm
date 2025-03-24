e0f91ecb1eaf0eff72d5c641b90c42d68d8dacc2
pchchv <ipchchv@gmail.com>
2025-03-24 08:19:50 +0300

    Add a Date type

--- note.go
+++ note.go
@@ -1,9 +1,17 @@
 package osm
 
+import "time"
+
 // NoteID is the unique identifier for an osm note.
 type NoteID int64
 
 // ObjectID is a helper returning the object id for this note id.
 func (id NoteID) ObjectID() ObjectID {
 	return ObjectID(noteMask | (id << versionBits))
+}
+
+// Date is an object to decode the date format used in the osm notes xml api.
+// Format: '2006-01-02 15:04:05 MST'.
+type Date struct {
+	time.Time
 }
