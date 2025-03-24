c95e353bc08a53194faf8a3eeff0c028a2f1cbe8
pchchv <ipchchv@gmail.com>
2025-03-24 09:00:20 +0300

    Add a ChangesetComment type

--- changeset.go
+++ changeset.go
@@ -1,9 +1,19 @@
 package osm
 
+import "time"
+
 // ChangesetID is the primary key for an osm changeset.
 type ChangesetID int64
 
 // ObjectID is a helper returning the object id for this changeset id.
 func (id ChangesetID) ObjectID() ObjectID {
 	return ObjectID(changesetMask | (id << versionBits))
+}
+
+// ChangesetComment is a specific comment in a changeset discussion.
+type ChangesetComment struct {
+	User      string    `xml:"user,attr" json:"user"`
+	UserID    UserID    `xml:"uid,attr" json:"uid"`
+	Timestamp time.Time `xml:"date,attr" json:"date"`
+	Text      string    `xml:"text" json:"text"`
 }
