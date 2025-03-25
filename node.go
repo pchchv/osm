2f78c06d5acc4ad200d5d9348e9d4f6dbd7d8836
pchchv <ipchchv@gmail.com>
2025-03-25 09:06:32 +0300

    Add a Node type

--- node.go
+++ node.go
@@ -1,5 +1,7 @@
 package osm
 
+import "time"
+
 // NodeID corresponds the primary key of a node.
 // The node id + version uniquely identify a node.
 type NodeID int64
@@ -17,4 +19,20 @@ func (id NodeID) FeatureID() FeatureID {
 // ElementID is a helper to convert the id to an element id.
 func (id NodeID) ElementID(v int) ElementID {
 	return id.FeatureID().ElementID(v)
+}
+
+// Node is an osm point and allows for marshalling to/from osm xml.
+type Node struct {
+	XMLName     xmlNameJSONTypeNode `xml:"node" json:"type"`
+	ID          NodeID              `xml:"id,attr" json:"id"`
+	Lat         float64             `xml:"lat,attr" json:"lat"`
+	Lon         float64             `xml:"lon,attr" json:"lon"`
+	User        string              `xml:"user,attr" json:"user,omitempty"`
+	UserID      UserID              `xml:"uid,attr" json:"uid,omitempty"`
+	Visible     bool                `xml:"visible,attr" json:"visible"`
+	Version     int                 `xml:"version,attr" json:"version,omitempty"`
+	ChangesetID ChangesetID         `xml:"changeset,attr" json:"changeset,omitempty"`
+	Timestamp   time.Time           `xml:"timestamp,attr" json:"timestamp"`
+	Tags        Tags                `xml:"tag" json:"tags,omitempty"`
+	Committed   *time.Time          `xml:"committed,attr,omitempty" json:"committed,omitempty"` // the estimated time this object was committed and made visible in the central OSM database
 }
