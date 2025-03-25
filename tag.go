bf89184b1c62518e82c96777183ed51938119c0e
pchchv <ipchchv@gmail.com>
2025-03-25 08:51:11 +0300

    Implement Map and Find methods for type Tags

--- tag.go
+++ tag.go
@@ -11,3 +11,24 @@ type Tag struct {
 // with some helper functions.
 type Tags []Tag
 
+// Map returns the tags as a key/value map.
+func (ts Tags) Map() map[string]string {
+	result := make(map[string]string, len(ts))
+	for _, t := range ts {
+		result[t.Key] = t.Value
+	}
+
+	return result
+}
+
+// Find returns the value for the key.
+// Returns empty string if not found.
+func (ts Tags) Find(k string) string {
+	for _, t := range ts {
+		if t.Key == k {
+			return t.Value
+		}
+	}
+
+	return ""
+}
