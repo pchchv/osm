21ab8a9db80158539220b872def18ade4211c064
pchchv <ipchchv@gmail.com>
2025-03-26 10:03:02 +0300

    Add unit tests for Relation type

--- relation_test.go
+++ relation_test.go
@@ -1,8 +1,13 @@
 package osm
 
 import (
+	"bytes"
+	"encoding/json"
 	"reflect"
 	"testing"
+	"time"
+
+	"github.com/pchchv/geo"
 )
 
 func TestMember_ids(t *testing.T) {
@@ -67,5 +72,130 @@ func TestMembers_ids(t *testing.T) {
 	}
 	if ids := ms.FeatureIDs(); !reflect.DeepEqual(ids, fids) {
 		t.Errorf("incorrect feature ids: %v", ids)
+	}
+}
+
+func TestRelation_ids(t *testing.T) {
+	r := Relation{ID: 12, Version: 2}
+	if id := r.FeatureID(); id != RelationID(12).FeatureID() {
+		t.Errorf("incorrect feature id: %v", id)
+	}
+
+	if id := r.ElementID(); id != RelationID(12).ElementID(2) {
+		t.Errorf("incorrect element id: %v", id)
+	}
+}
+
+func TestRelation_MarshalJSON(t *testing.T) {
+	r := Relation{
+		ID: 123,
+	}
+	data, err := json.Marshal(r)
+	if err != nil {
+		t.Fatalf("marshal error: %v", err)
+	}
+
+	if !bytes.Equal(data, []byte(`{"type":"relation","id":123,"visible":false,"timestamp":"0001-01-01T00:00:00Z","members":[]}`)) {
+		t.Errorf("incorrect json: %v", string(data))
+	}
+
+	// with members
+	r = Relation{
+		ID:      123,
+		Members: Members{{Type: "node", Ref: 123, Role: "outer", Version: 1}},
+	}
+
+	data, err = json.Marshal(r)
+	if err != nil {
+		t.Fatalf("marshal error: %v", err)
+	}
+
+	if !bytes.Equal(data, []byte(`{"type":"relation","id":123,"visible":false,"timestamp":"0001-01-01T00:00:00Z","members":[{"type":"node","ref":123,"role":"outer","version":1}]}`)) {
+		t.Errorf("incorrect json: %v", string(data))
+	}
+}
+
+func TestRelation_ApplyUpdatesUpTo(t *testing.T) {
+	updates := Updates{
+		{Index: 0, Timestamp: time.Date(2012, 1, 1, 0, 0, 0, 0, time.UTC), Version: 11},
+		{Index: 1, Timestamp: time.Date(2014, 1, 1, 0, 0, 0, 0, time.UTC), Version: 12},
+		{Index: 2, Timestamp: time.Date(2013, 1, 1, 0, 0, 0, 0, time.UTC), Version: 13, Lat: 10, Lon: 20},
+	}
+	r := Relation{
+		ID:      123,
+		Members: Members{{Version: 1}, {Version: 2}, {Version: 3}},
+	}
+
+	r.Updates = updates
+	r.ApplyUpdatesUpTo(time.Date(2011, 1, 1, 0, 0, 0, 0, time.UTC))
+	if r.Members[0].Version != 1 || r.Members[1].Version != 2 || r.Members[2].Version != 3 {
+		t.Errorf("incorrect members, got %v", r.Members)
+	}
+
+	r.Updates = updates
+	r.ApplyUpdatesUpTo(time.Date(2013, 1, 1, 0, 0, 0, 0, time.UTC))
+	if r.Members[0].Version != 11 || r.Members[1].Version != 2 || r.Members[2].Version != 13 {
+		t.Errorf("incorrect members, got %v", r.Members)
+	}
+
+	if r.Members[2].Lat != 10 {
+		t.Errorf("did not apply lat data")
+	}
+
+	if r.Members[2].Lon != 20 {
+		t.Errorf("did not apply lon data")
+	}
+
+	if l := len(r.Updates); l != 1 {
+		t.Errorf("incorrect number of updates: %v", l)
+	}
+
+	if r.Updates[0].Index != 1 {
+		t.Errorf("incorrect updates: %v", r.Updates)
+	}
+}
+
+func TestRelation_ApplyUpdate(t *testing.T) {
+	r := Relation{
+		ID:      123,
+		Members: Members{{Ref: 1, Type: TypeWay, Orientation: geo.CW}},
+	}
+	err := r.applyUpdate(Update{
+		Index:       0,
+		Version:     1,
+		ChangesetID: 2,
+		Reverse:     true,
+	})
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+
+	expected := Member{
+		Ref:         1,
+		Type:        TypeWay,
+		Version:     1,
+		ChangesetID: 2,
+		Orientation: geo.CCW,
+	}
+
+	if !reflect.DeepEqual(r.Members[0], expected) {
+		t.Errorf("incorrect update, got %v", r.Members[0])
+	}
+}
+
+func TestRelation_ApplyUpdate_error(t *testing.T) {
+	r := Relation{
+		ID:      123,
+		Members: Members{{Ref: 1, Type: TypeNode}},
+	}
+	err := r.applyUpdate(Update{
+		Index: 1,
+	})
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+
+	if e, ok := err.(*UpdateIndexOutOfRangeError); !ok {
+		t.Errorf("incorrect error, got %v", e)
 	}
 }
