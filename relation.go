cd143cd3f90de8d3f9dd2b16bf8f59df97d4d010
pchchv <ipchchv@gmail.com>
2025-03-26 09:50:55 +0300

    Add a Member type

--- relation.go
+++ relation.go
@@ -1,5 +1,7 @@
 package osm
 
+import "github.com/pchchv/geo"
+
 // RelationID is the primary key of a relation.
 // Relation is uniquely identifiable by the id + version.
 type RelationID int64
@@ -17,4 +19,24 @@ func (id RelationID) ObjectID(v int) ObjectID {
 // ElementID is a helper to convert the id to an element id.
 func (id RelationID) ElementID(v int) ElementID {
 	return id.FeatureID().ElementID(v)
+}
+
+// Member is a member of a relation.
+type Member struct {
+	Type        Type        `xml:"type,attr" json:"type"`
+	Ref         int64       `xml:"ref,attr" json:"ref"`
+	Role        string      `xml:"role,attr" json:"role"`
+	Version     int         `xml:"version,attr,omitempty" json:"version,omitempty"`
+	ChangesetID ChangesetID `xml:"changeset,attr,omitempty" json:"changeset,omitempty"`
+	// Node location if Type == Node
+	// Closest vertex to centroid if Type == Way
+	// Empty/invalid if Type == Relation
+	Lat float64 `xml:"lat,attr,omitempty" json:"lat,omitempty"`
+	Lon float64 `xml:"lon,attr,omitempty" json:"lon,omitempty"`
+	// Orientation is the direction of the way around a ring of a multipolygon.
+	// Only valid for multipolygon or boundary relations.
+	Orientation geo.Orientation `xml:"orientation,attr,omitempty" json:"orientation,omitempty"`
+	// Nodes are sometimes included in members of type way to include the lat/lon
+	// path of the way. Overpass returns xml like this.
+	Nodes WayNodes `xml:"nd" json:"nodes,omitempty"`
 }
