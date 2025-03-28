aa4083b4ab0bba8e6458ed304f66a3859e109b8e
pchchv <ipchchv@gmail.com>
2025-03-28 10:04:38 +0300

    Add a Changeset type

--- changeset.go
+++ changeset.go
@@ -13,6 +13,26 @@ func (id ChangesetID) ObjectID() ObjectID {
 	return ObjectID(changesetMask | (id << versionBits))
 }
 
+// Changeset is a set of metadata around a set of osm changes.
+type Changeset struct {
+	XMLName       xmlNameJSONTypeCS    `xml:"changeset" json:"type"`
+	ID            ChangesetID          `xml:"id,attr" json:"id"`
+	User          string               `xml:"user,attr" json:"user,omitempty"`
+	UserID        UserID               `xml:"uid,attr" json:"uid,omitempty"`
+	CreatedAt     time.Time            `xml:"created_at,attr" json:"created_at"`
+	ClosedAt      time.Time            `xml:"closed_at,attr" json:"closed_at"`
+	Open          bool                 `xml:"open,attr" json:"open"`
+	ChangesCount  int                  `xml:"num_changes,attr,omitempty" json:"num_changes,omitempty"`
+	MinLat        float64              `xml:"min_lat,attr" json:"min_lat,omitempty"`
+	MaxLat        float64              `xml:"max_lat,attr" json:"max_lat,omitempty"`
+	MinLon        float64              `xml:"min_lon,attr" json:"min_lon,omitempty"`
+	MaxLon        float64              `xml:"max_lon,attr" json:"max_lon,omitempty"`
+	CommentsCount int                  `xml:"comments_count,attr,omitempty" json:"comments_count,omitempty"`
+	Tags          Tags                 `xml:"tag" json:"tags,omitempty"`
+	Discussion    *ChangesetDiscussion `xml:"discussion,omitempty" json:"discussion,omitempty"`
+	Change        *Change              `xml:"-" json:"change,omitempty"`
+}
+
 // ChangesetComment is a specific comment in a changeset discussion.
 type ChangesetComment struct {
 	User      string    `xml:"user,attr" json:"user"`
