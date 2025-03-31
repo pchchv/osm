3969491a81505ebd248802129a78d2f558c544ad
pchchv <ipchchv@gmail.com>
2025-03-31 10:50:43 +0300

    Add polygonJSON initialization and sorting for polyConditions

--- polygon.go
+++ polygon.go
@@ -1,10 +1,171 @@
 package osm
 
+import (
+	"encoding/json"
+	"sort"
+)
+
 var (
 	polyConditions     []polyCondition
 	conditionAll       conditionType = "all"
 	conditionBlacklist conditionType = "blacklist"
 	conditionWhitelist conditionType = "whitelist"
+	// polygonJSON holds advanced conditions for when an osm way is a polygon.
+	// Sourced from: https://wiki.openstreetmap.org/wiki/Overpass_turbo/Polygon_Features
+	polygonJSON = []byte(`
+[
+    {
+        "key": "building",
+        "polygon": "all"
+    },
+    {
+        "key": "highway",
+        "polygon": "whitelist",
+        "values": [
+            "services",
+            "rest_area",
+            "escape",
+            "elevator"
+        ]
+    },
+    {
+        "key": "natural",
+        "polygon": "blacklist",
+        "values": [
+            "coastline",
+            "cliff",
+            "ridge",
+            "arete",
+            "tree_row"
+        ]
+    },
+    {
+        "key": "landuse",
+        "polygon": "all"
+    },
+    {
+        "key": "waterway",
+        "polygon": "whitelist",
+        "values": [
+            "riverbank",
+            "dock",
+            "boatyard",
+            "dam"
+        ]
+    },
+    {
+        "key": "amenity",
+        "polygon": "all"
+    },
+    {
+        "key": "leisure",
+        "polygon": "all"
+    },
+    {
+        "key": "barrier",
+        "polygon": "whitelist",
+        "values": [
+            "city_wall",
+            "ditch",
+            "hedge",
+            "retaining_wall",
+            "wall",
+            "spikes"
+        ]
+    },
+    {
+        "key": "railway",
+        "polygon": "whitelist",
+        "values": [
+            "station",
+            "turntable",
+            "roundhouse",
+            "platform"
+        ]
+    },
+    {
+        "key": "boundary",
+        "polygon": "all"
+    },
+    {
+        "key": "man_made",
+        "polygon": "blacklist",
+        "values": [
+            "cutline",
+            "embankment",
+            "pipeline"
+        ]
+    },
+    {
+        "key": "power",
+        "polygon": "whitelist",
+        "values": [
+            "plant",
+            "substation",
+            "generator",
+            "transformer"
+        ]
+    },
+    {
+        "key": "place",
+        "polygon": "all"
+    },
+    {
+        "key": "shop",
+        "polygon": "all"
+    },
+    {
+        "key": "aeroway",
+        "polygon": "blacklist",
+        "values": [
+            "taxiway"
+        ]
+    },
+    {
+        "key": "tourism",
+        "polygon": "all"
+    },
+    {
+        "key": "historic",
+        "polygon": "all"
+    },
+    {
+        "key": "public_transport",
+        "polygon": "all"
+    },
+    {
+        "key": "office",
+        "polygon": "all"
+    },
+    {
+        "key": "building:part",
+        "polygon": "all"
+    },
+    {
+        "key": "military",
+        "polygon": "all"
+    },
+    {
+        "key": "ruins",
+        "polygon": "all"
+    },
+    {
+        "key": "area:highway",
+        "polygon": "all"
+    },
+    {
+        "key": "craft",
+        "polygon": "all"
+    },
+    {
+        "key": "golf",
+        "polygon": "all"
+    },
+    {
+        "key": "indoor",
+        "polygon": "all"
+    }
+]`)
 )
 
 type conditionType string
@@ -13,4 +174,15 @@ type polyCondition struct {
 	Key       string        `json:"key"`
 	Condition conditionType `json:"polygon"`
 	Values    []string      `json:"values"`
+}
+
+func init() {
+	if err := json.Unmarshal(polygonJSON, &polyConditions); err != nil {
+		// must be valid json
+		panic(err)
+	}
+
+	for _, p := range polyConditions {
+		sort.StringSlice(p.Values).Sort()
+	}
 }
