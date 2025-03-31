a01a9aa95d90b4559a3fea483d2859d654fbebb8
pchchv <ipchchv@gmail.com>
2025-03-31 11:00:15 +0300

    Implement MarshalXML and UnmarshalXML methods

--- diff.go
+++ diff.go
@@ -1,5 +1,7 @@
 package osm
 
+import "encoding/xml"
+
 // The different types of diff actions.
 const (
 	ActionCreate ActionType = "create"
@@ -22,4 +24,87 @@ type Action struct {
 	*OSM `xml:",omitempty"`
 	Old  *OSM `xml:"old,omitempty"`
 	New  *OSM `xml:"new,omitempty"`
+}
+
+// MarshalXML converts a diff action to xml creating the proper structures.
+func (a Action) MarshalXML(e *xml.Encoder, start xml.StartElement) (err error) {
+	start.Attr = append(start.Attr, xml.Attr{Name: xml.Name{Local: "type"}, Value: string(a.Type)})
+	if err = e.EncodeToken(start); err != nil {
+		return
+	}
+
+	if a.OSM != nil {
+		if err = a.OSM.marshalInnerElementsXML(e); err != nil {
+			return
+		}
+	}
+
+	if a.Old != nil {
+		if err = marshalInnerChange(e, "old", a.Old); err != nil {
+			return
+		}
+	}
+
+	if a.New != nil {
+		if err = marshalInnerChange(e, "new", a.New); err != nil {
+			return
+		}
+	}
+
+	return e.EncodeToken(start.End())
+}
+
+// UnmarshalXML converts xml into a diff action.
+func (a *Action) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	for _, attr := range start.Attr {
+		if attr.Name.Local == "type" {
+			a.Type = ActionType(attr.Value)
+			break
+		}
+	}
+
+	for {
+		token, err := d.Token()
+		if err != nil {
+			break
+		}
+
+		start, ok := token.(xml.StartElement)
+		if !ok {
+			continue
+		}
+
+		switch start.Name.Local {
+		case "old":
+			a.Old = &OSM{}
+			if err := d.DecodeElement(a.Old, &start); err != nil {
+				return err
+			}
+		case "new":
+			a.New = &OSM{}
+			if err := d.DecodeElement(a.New, &start); err != nil {
+				return err
+			}
+		case "node":
+			n := &Node{}
+			if err := d.DecodeElement(&n, &start); err != nil {
+				return err
+			}
+			a.OSM = &OSM{Nodes: Nodes{n}}
+		case "way":
+			w := &Way{}
+			if err := d.DecodeElement(&w, &start); err != nil {
+				return err
+			}
+			a.OSM = &OSM{Ways: Ways{w}}
+		case "relation":
+			r := &Relation{}
+			if err := d.DecodeElement(&r, &start); err != nil {
+				return err
+			}
+			a.OSM = &OSM{Relations: Relations{r}}
+		}
+	}
+
+	return nil
 }
