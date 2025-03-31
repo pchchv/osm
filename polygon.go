3b71ebe7ca583888ce49d1fea06d2a6d024647ed
pchchv <ipchchv@gmail.com>
2025-03-31 10:48:26 +0300

    Add polyCondition type

--- polygon.go
+++ polygon.go
@@ -1,9 +1,16 @@
 package osm
 
 var (
+	polyConditions     []polyCondition
 	conditionAll       conditionType = "all"
 	conditionBlacklist conditionType = "blacklist"
 	conditionWhitelist conditionType = "whitelist"
 )
 
 type conditionType string
+
+type polyCondition struct {
+	Key       string        `json:"key"`
+	Condition conditionType `json:"polygon"`
+	Values    []string      `json:"values"`
+}
