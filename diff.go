ffb41736bfb4bb56699a63784b382f9ecdb72769
pchchv <ipchchv@gmail.com>
2025-03-31 10:57:09 +0300

    Add an Action type

--- diff.go
+++ diff.go
@@ -9,3 +9,17 @@ const (
 
 // ActionType is a strong type for the different diff actions.
 type ActionType string
+
+// Action is an explicit create,
+// modify or delete action with old and new data if applicable.
+// Different properties of this struct will be populated depending on the action.
+//
+//	Create: da.OSM will contain the new element
+//	Modify: da.Old and da.New will contain the old and new elements.
+//	Delete: da.Old and da.New will contain the old and new elements.
+type Action struct {
+	Type ActionType `xml:"type,attr"`
+	*OSM `xml:",omitempty"`
+	Old  *OSM `xml:"old,omitempty"`
+	New  *OSM `xml:"new,omitempty"`
+}
