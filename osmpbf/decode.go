596c0dcca07c6dfe0f487701bfe0b7c708a85231
pchchv <ipchchv@gmail.com>
2025-04-01 09:30:24 +0300

    Add a Header type

---
+++ osmpbf/decode.go
@@ -0,0 +1,19 @@
+package osmpbf
+
+import (
+	"time"
+
+	"github.com/pchchv/osm"
+)
+
+// Header contains the contents of the header in the pbf file.
+type Header struct {
+	Bounds               *osm.Bounds
+	RequiredFeatures     []string
+	OptionalFeatures     []string
+	WritingProgram       string
+	Source               string
+	ReplicationTimestamp time.Time
+	ReplicationSeqNum    uint64
+	ReplicationBaseURL   string
+}
