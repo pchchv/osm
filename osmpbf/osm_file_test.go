6ad932d4ad45766e9baaf5ae1e3c8c66fab5b551
pchchv <ipchchv@gmail.com>
2025-04-01 10:57:09 +0300

    Implement downloadTestOSMFile method

--- osmpbf/osm_file_test.go
+++ osmpbf/osm_file_test.go
@@ -1,6 +1,9 @@
 package osmpbf
 
 import (
+	"io"
+	"net/http"
+	"os"
 	"testing"
 
 	"github.com/pchchv/osm"
@@ -15,4 +18,30 @@ type OSMFileTest struct {
 	ExpRel                                 *osm.Relation
 	ExpNodeCount, ExpWayCount, ExpRelCount uint64
 	IDsExpOrder                            []string
+}
+
+func (ft *OSMFileTest) downloadTestOSMFile() {
+	if _, err := os.Stat(ft.FileName); os.IsNotExist(err) {
+		out, err := os.Create(ft.FileName)
+		if err != nil {
+			ft.Fatal(err)
+		}
+		defer out.Close()
+
+		resp, err := http.Get(ft.FileURL)
+		if err != nil {
+			ft.Fatal(err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			ft.Fatalf("test status code invalid: %v", resp.StatusCode)
+		}
+
+		if _, err := io.Copy(out, resp.Body); err != nil {
+			ft.Fatal(err)
+		}
+	} else if err != nil {
+		ft.Fatal(err)
+	}
 }
