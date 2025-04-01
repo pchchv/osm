5ebb162704b2452dd07597e76d99ba184db63998
pchchv <ipchchv@gmail.com>
2025-04-01 10:29:08 +0300

    Implement scanTags and extractMembers functions

--- osmpbf/decode_data.go
+++ osmpbf/decode_data.go
@@ -30,3 +30,67 @@ type dataDecoder struct {
 	lons           *pbr.Iterator
 	keyvals        *pbr.Iterator
 }
+
+// **NOTE**, it is assumed that keys and vals have the
+// same length and that the index is within the range of stringTable.
+func scanTags(stringTable []string, keys, vals *pbr.Iterator) (osm.Tags, error) {
+	var index int
+	tags := make(osm.Tags, keys.Count(pbr.WireTypeVarint))
+	for keys.HasNext() {
+		k, err := keys.Uint32()
+		if err != nil {
+			return nil, err
+		}
+
+		v, err := vals.Uint32()
+		if err != nil {
+			return nil, err
+		}
+
+		tags[index] = osm.Tag{
+			Key:   stringTable[k],
+			Value: stringTable[v],
+		}
+		index++
+	}
+
+	return tags, nil
+}
+
+// Makes relation members from a stringtable and three parallel arrays of IDs.
+func extractMembers(st []string, roles *pbr.Iterator, memids *pbr.Iterator, types *pbr.Iterator) (osm.Members, error) {
+	var index, memID int64
+	members := make(osm.Members, types.Count(pbr.WireTypeVarint))
+	for roles.HasNext() {
+		r, err := roles.Int32()
+		if err != nil {
+			return nil, err
+		}
+
+		members[index].Role = st[r]
+		m, err := memids.Sint64()
+		if err != nil {
+			return nil, err
+		}
+
+		memID += m
+		members[index].Ref = memID
+		t, err := types.Int32()
+		if err != nil {
+			return nil, err
+		}
+
+		switch osmpbf.Relation_MemberType(t) {
+		case osmpbf.Relation_NODE:
+			members[index].Type = osm.TypeNode
+		case osmpbf.Relation_WAY:
+			members[index].Type = osm.TypeWay
+		case osmpbf.Relation_RELATION:
+			members[index].Type = osm.TypeRelation
+		}
+
+		index++
+	}
+
+	return members, nil
+}
