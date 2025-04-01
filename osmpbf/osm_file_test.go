902dd1b012c58871b8863a41c3d3033a6a84a032
pchchv <ipchchv@gmail.com>
2025-04-01 10:59:26 +0300

    Implement a roundCoordinates function

--- osmpbf/osm_file_test.go
+++ osmpbf/osm_file_test.go
@@ -2,12 +2,15 @@ package osmpbf
 
 import (
 	"io"
+	"math"
 	"net/http"
 	"os"
 	"testing"
 
 	"github.com/pchchv/osm"
 )
+
+const coordinatesPrecision = 1e7
 
 type OSMFileTest struct {
 	*testing.T
@@ -43,5 +46,14 @@ func (ft *OSMFileTest) downloadTestOSMFile() {
 		}
 	} else if err != nil {
 		ft.Fatal(err)
+	}
+}
+
+func roundCoordinates(w *osm.Way) {
+	if w != nil {
+		for i := range w.Nodes {
+			w.Nodes[i].Lat = math.Round(w.Nodes[i].Lat*coordinatesPrecision) / coordinatesPrecision
+			w.Nodes[i].Lon = math.Round(w.Nodes[i].Lon*coordinatesPrecision) / coordinatesPrecision
+		}
 	}
 }
