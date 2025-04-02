6c2ee6929328a547db7a9c66256c66ba62b3ec8f
pchchv <ipchchv@gmail.com>
2025-04-02 10:21:36 +0300

    Implement Join function to connect segments into MultiSegments and add related tests

--- mputil/join.go
+++ mputil/join.go
@@ -1,5 +1,77 @@
 package mputil
 
+// Join joins a set of segments into a set of connected MultiSegments.
+func Join(segments []Segment) []MultiSegment {
+	lists := []MultiSegment{}
+	segments = compact(segments)
+	// matches are removed from `segments` and
+	// put into the current group,
+	// so when `segments` is empty its done
+	for len(segments) != 0 {
+		current := MultiSegment{segments[len(segments)-1]}
+		segments = segments[:len(segments)-1]
+		// if the current group is a ring, its done
+		// else add in all the lines
+		for len(segments) != 0 && !current.First().Equal(current.Last()) {
+			first := current.First()
+			last := current.Last()
+			foundAt := -1
+			for i, segment := range segments {
+				if last.Equal(segment.First()) {
+					// nice fit at the end of current
+					segment.Line = segment.Line[1:]
+					current = append(current, segment)
+					foundAt = i
+					break
+				} else if last.Equal(segment.Last()) {
+					// reverse it and it'll fit at the end
+					segment.Reverse()
+					segment.Line = segment.Line[1:]
+					current = append(current, segment)
+					foundAt = i
+					break
+				} else if first.Equal(segment.Last()) {
+					// nice fit at the start of current
+					segment.Line = segment.Line[:len(segment.Line)-1]
+					current = append(MultiSegment{segment}, current...)
+					foundAt = i
+					break
+				} else if first.Equal(segment.First()) {
+					// reverse it and it'll fit at the start
+					segment.Reverse()
+					segment.Line = segment.Line[:len(segment.Line)-1]
+					current = append(MultiSegment{segment}, current...)
+					foundAt = i
+					break
+				}
+			}
+
+			if foundAt == -1 {
+				break // Invalid geometry (dangling way, unclosed ring)
+			}
+
+			// remove the found/matched segment from the list.
+			if foundAt < len(segments)/2 {
+				// first half, shift up
+				for i := foundAt; i > 0; i-- {
+					segments[i] = segments[i-1]
+				}
+				segments = segments[1:]
+			} else {
+				// second half, shift down
+				for i := foundAt + 1; i < len(segments); i++ {
+					segments[i-1] = segments[i]
+				}
+				segments = segments[:len(segments)-1]
+			}
+		}
+
+		lists = append(lists, current)
+	}
+
+	return lists
+}
+
 func compact(ms MultiSegment) MultiSegment {
 	var at int
 	for _, s := range ms {
---
+++ mputil/join_test.go
@@ -0,0 +1,321 @@
+package mputil
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/pchchv/geo"
+)
+
+func TestJoin(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []Segment
+		output []MultiSegment
+	}{
+		{
+			name: "single line",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{0, 0}, {1, 1}},
+					},
+				},
+			},
+		},
+		{
+			name: "two loops",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}, {1, 2}, {0, 0}},
+				},
+				{
+					Line: geo.LineString{{1, 0}, {2, 1}, {2, 2}, {1, 0}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{1, 0}, {2, 1}, {2, 2}, {1, 0}},
+					},
+				},
+				{
+					{
+						Line: geo.LineString{{0, 0}, {1, 1}, {1, 2}, {0, 0}},
+					},
+				},
+			},
+		},
+		{
+			name: "joins two lines",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{1, 1}, {2, 2}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{0, 0}},
+					},
+					{
+						Line: geo.LineString{{1, 1}, {2, 2}},
+					},
+				},
+			},
+		},
+		{
+			name: "joins two lines reverse order",
+			input: []Segment{
+				{
+					Line: geo.LineString{{1, 1}, {2, 2}},
+				},
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{0, 0}, {1, 1}},
+					},
+					{
+						Line: geo.LineString{{2, 2}},
+					},
+				},
+			},
+		},
+		{
+			name: "joins two lines with reverse",
+			input: []Segment{
+				{
+					Line: geo.LineString{{2, 2}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{2, 2}, {3, 3}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Reversed: true,
+						Line:     geo.LineString{{1, 1}},
+					},
+					{
+						Line: geo.LineString{{2, 2}, {3, 3}},
+					},
+				},
+			},
+		},
+		{
+			name: "joins two lines with reverse second",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{2, 2}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{2, 2}, {1, 1}},
+					},
+					{
+						Reversed: true,
+						Line:     geo.LineString{{0, 0}},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Join(tc.input)
+			compareMultiSegment(t, result, tc.output)
+		})
+	}
+}
+
+func TestJoin_lineString_singlePointLine(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []Segment
+		output []MultiSegment
+	}{
+		{
+			name: "single point line, first",
+			input: []Segment{
+				{
+					Line: geo.LineString{{1, 1}},
+				},
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{0, 0}, {1, 1}},
+					},
+				},
+			},
+		},
+		{
+			name: "single point line, last",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{0, 0}, {1, 1}},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Join(tc.input)
+			compareMultiSegment(t, result, tc.output)
+		})
+	}
+}
+
+func TestJoin_lineString_danglingLine(t *testing.T) {
+	cases := []struct {
+		name   string
+		input  []Segment
+		output []MultiSegment
+	}{
+		{
+			name: "dangling line, last",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{2, 2}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{3, 3}, {4, 4}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{3, 3}, {4, 4}},
+					},
+				},
+				{
+					{
+						Line: geo.LineString{{2, 2}, {1, 1}},
+					},
+					{
+						Reversed: true,
+						Line:     geo.LineString{{0, 0}},
+					},
+				},
+			},
+		},
+		{
+			name: "dangling line, middle",
+			input: []Segment{
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{3, 3}, {4, 4}},
+				},
+				{
+					Line: geo.LineString{{2, 2}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{2, 2}, {1, 1}},
+					},
+					{
+						Reversed: true,
+						Line:     geo.LineString{{0, 0}},
+					},
+				},
+				{
+					{
+						Line: geo.LineString{{3, 3}, {4, 4}},
+					},
+				},
+			},
+		},
+		{
+			name: "dangling line, first",
+			input: []Segment{
+				{
+					Line: geo.LineString{{3, 3}, {4, 4}},
+				},
+				{
+					Line: geo.LineString{{0, 0}, {1, 1}},
+				},
+				{
+					Line: geo.LineString{{2, 2}, {1, 1}},
+				},
+			},
+			output: []MultiSegment{
+				{
+					{
+						Line: geo.LineString{{2, 2}, {1, 1}},
+					},
+					{
+						Reversed: true,
+						Line:     geo.LineString{{0, 0}},
+					},
+				},
+				{
+					{
+						Line: geo.LineString{{3, 3}, {4, 4}},
+					},
+				},
+			},
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			result := Join(tc.input)
+			compareMultiSegment(t, result, tc.output)
+		})
+	}
+}
+
+func compareMultiSegment(t testing.TB, result, expected []MultiSegment) {
+	t.Helper()
+	if len(result) != len(expected) {
+		t.Fatalf("length mismatch: %v != %v", len(result), len(expected))
+	}
+
+	for i, sm := range result {
+		if !reflect.DeepEqual(sm, expected[i]) {
+			t.Errorf("segment %d did not match", i)
+			t.Logf("%v", sm)
+			t.Logf("%v", expected[i])
+		}
+	}
+}
