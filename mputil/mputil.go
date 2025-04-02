53d937a99fe16095ba2954e0e5a5048cd5883db4
pchchv <ipchchv@gmail.com>
2025-04-02 09:57:29 +0300

    Implement First, Last and Reverse methods

--- mputil/mputil.go
+++ mputil/mputil.go
@@ -11,3 +11,21 @@ type Segment struct {
 	Orientation geo.Orientation
 	Line        geo.LineString
 }
+
+// First returns the first point in
+// the segment linestring.
+func (s Segment) First() geo.Point {
+	return s.Line[0]
+}
+
+// Last returns the last point in
+// the segment linestring.
+func (s Segment) Last() geo.Point {
+	return s.Line[len(s.Line)-1]
+}
+
+// Reverse reverses the line string of the segment.
+func (s *Segment) Reverse() {
+	s.Reversed = !s.Reversed
+	s.Line.Reverse()
+}
