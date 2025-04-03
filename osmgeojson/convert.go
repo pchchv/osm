b03ffda557f9977e14266b806669af2263d64cf8
pchchv <ipchchv@gmail.com>
2025-04-03 09:30:22 +0300

    Add context type

--- osmgeojson/convert.go
+++ osmgeojson/convert.go
@@ -7,3 +7,16 @@ type relationSummary struct {
 	Role string            `json:"role"`
 	Tags map[string]string `json:"tags"`
 }
+
+type context struct {
+	noID                   bool
+	noMeta                 bool
+	noRelationMembership   bool
+	includeInvalidPolygons bool
+	osm                    *osm.OSM
+	wayMap                 map[osm.WayID]*osm.Way
+	skippable              map[osm.WayID]struct{}
+	wayMember              map[osm.NodeID]struct{}
+	nodeMap                map[osm.NodeID]*osm.Node
+	relationMember         map[osm.FeatureID][]*relationSummary
+}
