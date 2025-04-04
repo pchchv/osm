cde8b197356cd0a49e5f32954b41b72a0aeeb298
pchchv <ipchchv@gmail.com>
2025-04-04 13:26:13 +0300

    Implement At function to support time-based feature requests

--- osmapi/options.go
+++ osmapi/options.go
@@ -18,6 +18,12 @@ func (o *at) applyFeature(p []string) ([]string, error) {
 	return append(p, "at="+o.t.UTC().Format("2006-01-02T15:04:05Z")), nil
 }
 
+// At adds an `at=2006-01-02T15:04:05Z` parameter to the request.
+// The osm.fyi supports requesting features and maps as they were at the given time.
+func At(t time.Time) FeatureOption {
+	return &at{t}
+}
+
 func featureOptions(opts []FeatureOption) (string, error) {
 	if len(opts) == 0 {
 		return "", nil
