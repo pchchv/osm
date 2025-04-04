b555f5db7f248c5005d45e1ca498e5af4decf34b
pchchv <ipchchv@gmail.com>
2025-04-04 13:25:28 +0300

    Add an at type

--- osmapi/options.go
+++ osmapi/options.go
@@ -1,10 +1,21 @@
 package osmapi
 
-import "strings"
+import (
+	"strings"
+	"time"
+)
 
 // FeatureOption used when fetching a feature or a set of different features.
 type FeatureOption interface {
 	applyFeature([]string) ([]string, error)
+}
+
+type at struct {
+	t time.Time
+}
+
+func (o *at) applyFeature(p []string) ([]string, error) {
+	return append(p, "at="+o.t.UTC().Format("2006-01-02T15:04:05Z")), nil
 }
 
 func featureOptions(opts []FeatureOption) (string, error) {
