787f81c67de621f4887dd668e04884c0c27aac4b
pchchv <ipchchv@gmail.com>
2025-04-04 13:04:02 +0300

    Add Datasource type with its constructor

--- osmapi/datasource.go
+++ osmapi/datasource.go
@@ -1,6 +1,9 @@
 package osmapi
 
-import "context"
+import (
+	"context"
+	"net/http"
+)
 
 // RateLimiter waits until the next allowed request.
 // This interface is met by `golang.org/x/time/rate.Limiter`
@@ -11,4 +14,22 @@ import "context"
 //	osmapi.DefaultDatasource.Limiter = rate.NewLimiter(10, 1)
 type RateLimiter interface {
 	Wait(context.Context) error
+}
+
+// Datasource defines context about the http client to use to make requests.
+type Datasource struct {
+	// If Limiter is non-nil.
+	// The datasource will wait/block until the request is allowed by the rate limiter.
+	// It is good practice to use this parameter when making may
+	// concurrent requests against the prod osm api.
+	Limiter RateLimiter
+	BaseURL string
+	Client  *http.Client
+}
+
+// NewDatasource creates a Datasource using the given client.
+func NewDatasource(client *http.Client) *Datasource {
+	return &Datasource{
+		Client: client,
+	}
 }
