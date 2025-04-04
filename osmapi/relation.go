54c5260d77e1dd275752e4a1e23f80635deea019
pchchv <ipchchv@gmail.com>
2025-04-04 13:30:42 +0300

    Implement Relation and Relations methods for Datasource

---
+++ osmapi/relation.go
@@ -0,0 +1,58 @@
+package osmapi
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	"github.com/pchchv/osm"
+)
+
+// Relation returns the latest version of the relation from the osm rest api.
+func (ds *Datasource) Relation(ctx context.Context, id osm.RelationID, opts ...FeatureOption) (*osm.Relation, error) {
+	params, err := featureOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	o := &osm.OSM{}
+	url := fmt.Sprintf("%s/relation/%d?%s", ds.baseURL(), id, params)
+	if err := ds.getFromAPI(ctx, url, &o); err != nil {
+		return nil, err
+	}
+
+	if l := len(o.Relations); l != 1 {
+		return nil, fmt.Errorf("wrong number of relations, expected 1, got %v", l)
+	}
+
+	return o.Relations[0], nil
+}
+
+// Relations returns the latest version of the relations from the osm rest api.
+// Returns 404 if any node is missing.
+func (ds *Datasource) Relations(ctx context.Context, ids []osm.RelationID, opts ...FeatureOption) (osm.Relations, error) {
+	params, err := featureOptions(opts)
+	if err != nil {
+		return nil, err
+	}
+
+	data := make([]byte, 0, 11*len(ids))
+	for i, id := range ids {
+		if i != 0 {
+			data = append(data, byte(','))
+		}
+		data = strconv.AppendInt(data, int64(id), 10)
+	}
+
+	url := ds.baseURL() + "/relations?relations=" + string(data)
+	if len(params) > 0 {
+		url += "&" + params
+	}
+
+	o := &osm.OSM{}
+	if err := ds.getFromAPI(ctx, url, &o); err != nil {
+		return nil, err
+	}
+
+	return o.Relations, nil
+}
