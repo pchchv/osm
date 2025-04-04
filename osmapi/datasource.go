926edd882307aae8d43bd43be04f4dbde35d9507
pchchv <ipchchv@gmail.com>
2025-04-04 12:59:03 +0300

    Add RateLimiter type

--- osmapi/datasource.go
+++ osmapi/datasource.go
@@ -1,1 +1,14 @@
 package osmapi
+
+import "context"
+
+// RateLimiter waits until the next allowed request.
+// This interface is met by `golang.org/x/time/rate.Limiter`
+// and is meant to be used with it.
+// For example:
+//
+//	// 10 qps
+//	osmapi.DefaultDatasource.Limiter = rate.NewLimiter(10, 1)
+type RateLimiter interface {
+	Wait(context.Context) error
+}
