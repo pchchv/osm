f83073cd12170e01e7d6158c92fb7e0fc7acf71b
pchchv <ipchchv@gmail.com>
2025-04-07 09:15:32 +0300

    Implement Note and Notes retrieval functions in osmapi

---
+++ osmapi/note.go
@@ -0,0 +1,76 @@
+package osmapi
+
+import (
+	"context"
+	"fmt"
+	"net/url"
+	"strings"
+
+	"github.com/pchchv/osm"
+)
+
+var (
+	_ NotesOption = Limit(1)
+	_ NotesOption = MaxDaysClosed(1)
+)
+
+// Note returns the note from the osm rest api.
+func (ds *Datasource) Note(ctx context.Context, id osm.NoteID) (*osm.Note, error) {
+	o := &osm.OSM{}
+	url := fmt.Sprintf("%s/notes/%d", ds.baseURL(), id)
+	if err := ds.getFromAPI(ctx, url, &o); err != nil {
+		return nil, err
+	}
+
+	if l := len(o.Notes); l != 1 {
+		return nil, fmt.Errorf("wrong number of notes, expected 1, got %v", l)
+	}
+
+	return o.Notes[0], nil
+}
+
+// Notes returns the notes in a bounding box.
+// Can provide options to limit the results or change what it means to be "closed".
+// See the options or osm api v0.6 docs for details.
+func (ds *Datasource) Notes(ctx context.Context, bounds *osm.Bounds, opts ...NotesOption) (osm.Notes, error) {
+	var err error
+	params := make([]string, 0, 1+len(opts))
+	params = append(params, fmt.Sprintf("bbox=%f,%f,%f,%f", bounds.MinLon, bounds.MinLat, bounds.MaxLon, bounds.MaxLat))
+	for _, o := range opts {
+		params, err = o.applyNotes(params)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	o := &osm.OSM{}
+	url := fmt.Sprintf("%s/notes?%s", ds.baseURL(), strings.Join(params, "&"))
+	if err := ds.getFromAPI(ctx, url, &o); err != nil {
+		return nil, err
+	}
+
+	return o.Notes, nil
+}
+
+// NotesSearch returns the notes whose text matches the query.
+// Can provide options to limit the results or change what it means to be "closed".
+// See the options or osm api v0.6 docs for details.
+func (ds *Datasource) NotesSearch(ctx context.Context, query string, opts ...NotesOption) (osm.Notes, error) {
+	var err error
+	params := make([]string, 0, 1+len(opts))
+	params = append(params, fmt.Sprintf("q=%s", url.QueryEscape(query)))
+	for _, o := range opts {
+		params, err = o.applyNotes(params)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	o := &osm.OSM{}
+	url := fmt.Sprintf("%s/notes/search?%s", ds.baseURL(), strings.Join(params, "&"))
+	if err := ds.getFromAPI(ctx, url, &o); err != nil {
+		return nil, err
+	}
+
+	return o.Notes, nil
+}
