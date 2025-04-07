89b7a87722ae224f1e9411954d771ea29aa02d15
pchchv <ipchchv@gmail.com>
2025-04-07 09:08:21 +0300

    Implement ChangesetDownload and ChangesetWithDiscussion methods for Datasource

--- osmapi/changeset.go
+++ osmapi/changeset.go
@@ -13,6 +13,23 @@ func (ds *Datasource) Changeset(ctx context.Context, id osm.ChangesetID) (*osm.C
 	return ds.getChangeset(ctx, url)
 }
 
+// ChangesetDownload returns the full osmchange for the changeset using the osm rest api.
+func (ds *Datasource) ChangesetDownload(ctx context.Context, id osm.ChangesetID) (*osm.Change, error) {
+	change := &osm.Change{}
+	url := fmt.Sprintf("%s/changeset/%d/download", ds.baseURL(), id)
+	if err := ds.getFromAPI(ctx, url, &change); err != nil {
+		return nil, err
+	}
+
+	return change, nil
+}
+
+// ChangesetWithDiscussion returns a changeset and its discussion from the osm rest api.
+func (ds *Datasource) ChangesetWithDiscussion(ctx context.Context, id osm.ChangesetID) (*osm.Changeset, error) {
+	url := fmt.Sprintf("%s/changeset/%d?include_discussion=true", ds.baseURL(), id)
+	return ds.getChangeset(ctx, url)
+}
+
 func (ds *Datasource) getChangeset(ctx context.Context, url string) (*osm.Changeset, error) {
 	css := &osm.OSM{}
 	if err := ds.getFromAPI(ctx, url, &css); err != nil {
