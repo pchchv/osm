6524b2eefdf6b355b2998f726c78515d9f7f268f
pchchv <ipchchv@gmail.com>
2025-04-08 11:10:47 +0300

    Add Child type

--- annotate/shared/child.go
+++ annotate/shared/child.go
@@ -1,1 +1,23 @@
 package shared
+
+import (
+	"time"
+
+	"github.com/pchchv/osm"
+)
+
+// Child represents a node, way or relation that is a
+// dependent for annotating ways or relations.
+type Child struct {
+	ID                osm.FeatureID
+	Version           int
+	ChangesetID       osm.ChangesetID
+	VersionIndex      int // sorted version index (versions do not have to start with 1 or be sequential)
+	Timestamp         time.Time
+	Committed         time.Time
+	Lon               float64 // for nodes
+	Lat               float64
+	Way               *osm.Way // for ways
+	ReverseOfPrevious bool
+	Visible           bool
+}
