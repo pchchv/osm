c64b215048f13940700612d994b1bb61d04864ad
pchchv <ipchchv@gmail.com>
2025-04-08 10:38:44 +0300

    Add a ChildFirstOrdering type

--- annotate/order.go
+++ annotate/order.go
@@ -2,14 +2,32 @@ package annotate
 
 import (
 	"context"
+	"sync"
 
 	"github.com/pchchv/osm"
 )
 
 var _ RelationHistoryDatasourcer = &osm.HistoryDatasource{}
 
-// RelationHistoryDatasourcer is an more strict interface for when we only need the relation history.
+// RelationHistoryDatasourcer is a stricter interface for cases where only relationship history is needed.
 type RelationHistoryDatasourcer interface {
 	RelationHistory(context.Context, osm.RelationID) (osm.Relations, error)
 	NotFound(error) bool
+}
+
+// ChildFirstOrdering allows to process a set of relations in a dept first order.
+// Since relations can refer to other relations,
+// it must be ensured that children are added before parents.
+type ChildFirstOrdering struct {
+	// CompletedIndex is the number of relation ids in the provided array that have been finished.
+	// This can be used as a good restart position.
+	CompletedIndex int
+	ctx            context.Context
+	done           context.CancelFunc
+	ds             RelationHistoryDatasourcer
+	visited        map[osm.RelationID]struct{}
+	out            chan osm.RelationID
+	wg             sync.WaitGroup
+	id             osm.RelationID
+	err            error
 }
