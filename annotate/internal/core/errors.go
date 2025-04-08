fc7ebd76e775da5b3f5663d3a9f1cb862f7ebeb2
pchchv <ipchchv@gmail.com>
2025-04-08 12:07:49 +0300

    Add NoVisibleChildError and NoHistoryError types with error messages

---
+++ annotate/internal/core/errors.go
@@ -0,0 +1,29 @@
+package core
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/pchchv/osm"
+)
+
+// NoVisibleChildError is returned if there are no visible children for a parent at a given time.
+type NoVisibleChildError struct {
+	ChildID   osm.FeatureID
+	Timestamp time.Time
+}
+
+// Error returns a pretty string of the error.
+func (e *NoVisibleChildError) Error() string {
+	return fmt.Sprintf("no visible child for %v at %v", e.ChildID, e.Timestamp)
+}
+
+// NoHistoryError is returned if there is no entry in the history map for a specific child.
+type NoHistoryError struct {
+	ChildID osm.FeatureID
+}
+
+// Error returns a pretty string of the error.
+func (e *NoHistoryError) Error() string {
+	return fmt.Sprintf("element history not found for %v", e.ChildID)
+}
