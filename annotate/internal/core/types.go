db44c40f8be70a00f76c930bd44b945c06bc9db8
pchchv <ipchchv@gmail.com>
2025-04-08 11:20:32 +0300

    Implement absDuration and timeThreshold functions

--- annotate/internal/core/types.go
+++ annotate/internal/core/types.go
@@ -1,5 +1,26 @@
 package core
 
-import "github.com/pchchv/osm/annotate/shared"
+import (
+	"time"
+
+	"github.com/pchchv/osm"
+	"github.com/pchchv/osm/annotate/shared"
+)
 
 type ChildList []*shared.Child
+
+func absDuration(d time.Duration) time.Duration {
+	if d < 0 {
+		return -d
+	}
+
+	return d
+}
+
+func timeThreshold(c *shared.Child, esp time.Duration) time.Time {
+	if c.Committed.Before(osm.CommitInfoStart) {
+		return c.Timestamp.Add(esp)
+	}
+
+	return c.Committed
+}
