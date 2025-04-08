8a817f645b6167105d4ec97bcd5b9c25f62bb807
pchchv <ipchchv@gmail.com>
2025-04-08 11:11:48 +0300

    Implement constuctors for a Child type

--- annotate/shared/child.go
+++ annotate/shared/child.go
@@ -21,3 +21,57 @@ type Child struct {
 	ReverseOfPrevious bool
 	Visible           bool
 }
+
+// FromNode converts a node to a child.
+func FromNode(n *osm.Node) *Child {
+	c := &Child{
+		ID:          n.FeatureID(),
+		Version:     n.Version,
+		ChangesetID: n.ChangesetID,
+		Visible:     n.Visible,
+		Timestamp:   n.Timestamp,
+		Lon:         n.Lon,
+		Lat:         n.Lat,
+	}
+
+	if n.Committed != nil {
+		c.Committed = *n.Committed
+	}
+
+	return c
+}
+
+// FromWay converts a way to a child.
+func FromWay(w *osm.Way) *Child {
+	c := &Child{
+		ID:          w.FeatureID(),
+		Version:     w.Version,
+		ChangesetID: w.ChangesetID,
+		Visible:     w.Visible,
+		Timestamp:   w.Timestamp,
+		Way:         w,
+	}
+
+	if w.Committed != nil {
+		c.Committed = *w.Committed
+	}
+
+	return c
+}
+
+// FromRelation converts a way to a child.
+func FromRelation(r *osm.Relation) *Child {
+	c := &Child{
+		ID:          r.FeatureID(),
+		Version:     r.Version,
+		ChangesetID: r.ChangesetID,
+		Visible:     r.Visible,
+		Timestamp:   r.Timestamp,
+	}
+
+	if r.Committed != nil {
+		c.Committed = *r.Committed
+	}
+
+	return c
+}
