1011f2f1abbab1e24cbf99c5c30623093a379bc2
pchchv <ipchchv@gmail.com>
2025-04-08 11:01:18 +0300

    Implement wayCentroid and wayPointOnSurface functions

--- annotate/geo.go
+++ annotate/geo.go
@@ -1,9 +1,11 @@
 package annotate
 
 import (
+	"math"
 	"time"
 
 	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/geometries"
 	"github.com/pchchv/osm"
 	"github.com/pchchv/osm/mputil"
 )
@@ -38,4 +40,37 @@ func annotateOrientation(members osm.Members, ms mputil.MultiSegment, o geo.Orie
 			members[segment.Index].Orientation = factor * o
 		}
 	}
+}
+
+func wayCentroid(w *osm.Way) geo.Point {
+	var dist float64
+	point := geo.Point{}
+	seg := [2]geo.Point{}
+	for i := 0; i < len(w.Nodes)-1; i++ {
+		seg[0] = w.Nodes[i].Point()
+		seg[1] = w.Nodes[i+1].Point()
+		d := geometries.Distance(seg[0], seg[1])
+		point[0] += (seg[0][0] + seg[1][0]) / 2.0 * d
+		point[1] += (seg[0][1] + seg[1][1]) / 2.0 * d
+		dist += d
+	}
+
+	point[0] /= dist
+	point[1] /= dist
+
+	return point
+}
+
+// wayPointOnSurface finds closest node to centroid.
+func wayPointOnSurface(w *osm.Way) geo.Point {
+	var index int
+	centroid := wayCentroid(w)
+	min := math.MaxFloat64
+	for i, n := range w.Nodes {
+		if d := geometries.Distance(centroid, n.Point()); d < min {
+			index, min = i, d
+		}
+	}
+
+	return w.Nodes[index].Point()
 }
