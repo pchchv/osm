e7e07dbee70ab126f6179a6664c01b46343fec8f
pchchv <ipchchv@gmail.com>
2025-04-09 10:42:47 +0300

    Implement Get and MustGet methods

--- annotate/internal/core/datasourcer_test.go
+++ annotate/internal/core/datasourcer_test.go
@@ -1,8 +1,39 @@
 package core
 
-import "github.com/pchchv/osm"
+import (
+	"context"
+	"errors"
+
+	"github.com/pchchv/osm"
+)
+
+var ErrNotFound             = errors.New("not found")
 
 // TestDS implements a datasource for testing.
 type TestDS struct {
 	data map[osm.FeatureID]ChildList
+}
+
+// Get returns the history in ChildList form.
+func (tds *TestDS) Get(ctx context.Context, id osm.FeatureID) (ChildList, error) {
+	if tds.data == nil {
+		return nil, ErrNotFound
+	}
+
+	v := tds.data[id]
+	if v == nil {
+		return nil, ErrNotFound
+	}
+
+	return v, nil
+}
+
+// MustGet is used by tests only to simplify some code.
+func (tds *TestDS) MustGet(id osm.FeatureID) ChildList {
+	v, err := tds.Get(context.TODO(), id)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
 }
