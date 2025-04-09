6cb01e65245a8700e85c4eeac1f82697b6cb6767
pchchv <ipchchv@gmail.com>
2025-04-09 11:58:49 +0300

    Implement Timestamp, Committed and Visible methods

--- annotate/way.go
+++ annotate/way.go
@@ -1,6 +1,10 @@
 package annotate
 
-import "github.com/pchchv/osm"
+import (
+	"time"
+
+	"github.com/pchchv/osm"
+)
 
 // parentWay wraps a osm.Way into the
 // core.Parent interface so that updates can be computed.
@@ -18,4 +22,20 @@ func (w *parentWay) ID() osm.FeatureID {
 
 func (w *parentWay) ChangesetID() osm.ChangesetID {
 	return w.Way.ChangesetID
+}
+
+func (w *parentWay) Timestamp() time.Time {
+	return w.Way.Timestamp
+}
+
+func (w *parentWay) Committed() time.Time {
+	if w.Way.Committed == nil {
+		return time.Time{}
+	}
+
+	return *w.Way.Committed
+}
+
+func (w *parentWay) Visible() bool {
+	return w.Way.Visible
 }
