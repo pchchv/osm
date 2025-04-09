39c31dd9365f4dff3231464326ec878bfdda0bd8
pchchv <ipchchv@gmail.com>
2025-04-09 12:04:33 +0300

    Implement a Get method

--- annotate/datasource.go
+++ annotate/datasource.go
@@ -1,7 +1,22 @@
 package annotate
 
+import (
+	"context"
+
+	"github.com/pchchv/osm"
+	"github.com/pchchv/osm/annotate/internal/core"
+)
+
 type wayChildDatasource struct {
 	NodeHistoryAsChildrenDatasourcer
+}
+
+func (wds *wayChildDatasource) Get(ctx context.Context, id osm.FeatureID) (core.ChildList, error) {
+	if id.Type() != osm.TypeNode {
+		panic("only node types supported")
+	}
+
+	return wds.NodeHistoryAsChildren(ctx, id.NodeID())
 }
 
 type wayDatasource struct {
