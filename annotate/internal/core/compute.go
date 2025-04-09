a2ae758bdfa605dd0ba6b76e946b5548e59ce9a1
pchchv <ipchchv@gmail.com>
2025-04-09 10:38:44 +0300

    Enhance Compute function to handle missing children and data inconsistencies

--- annotate/internal/core/compute.go
+++ annotate/internal/core/compute.go
@@ -2,6 +2,7 @@ package core
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/pchchv/osm"
@@ -21,6 +22,110 @@ type Options struct {
 type Datasourcer interface {
 	Get(ctx context.Context, id osm.FeatureID) (ChildList, error)
 	NotFound(err error) bool
+}
+
+// Compute first computes the exact version of the children in each parent,
+// then returns a set of updates for each version of the parent.
+func Compute(ctx context.Context, parents []Parent, histories Datasourcer, opts *Options) ([]osm.Updates, error) {
+	if opts == nil {
+		opts = &Options{}
+	}
+
+	results := make([]osm.Updates, len(parents))
+	for fid, locations := range mapChildLocs(parents, opts.ChildFilter) {
+		child, err := histories.Get(ctx, fid)
+		if err != nil {
+			if !histories.NotFound(err) {
+				return nil, err
+			}
+
+			if opts.IgnoreMissingChildren {
+				continue
+			}
+
+			return nil, &NoHistoryError{ChildID: fid}
+		}
+
+		for _, locs := range locations.GroupByParent() {
+			// figure out the parent and the next parent
+			parentIndex := locs[0].Parent
+			parent := parents[parentIndex]
+			if !parent.Visible() {
+				continue
+			}
+
+			var nextParent Parent
+			if parentIndex < len(parents)-1 {
+				nextParent = parents[parentIndex+1]
+			}
+
+			// get the current child
+			c := child.FindVisible(
+				parent.ChangesetID(),
+				timeThresholdParent(parent, 0),
+				opts.Threshold,
+			)
+			if c == nil && !opts.IgnoreInconsistency {
+				return nil, &NoVisibleChildError{
+					ChildID:   fid,
+					Timestamp: timeThresholdParent(parent, 0)}
+			}
+
+			// straight up set this child on major version
+			for _, cl := range locs {
+				parent.SetChild(cl.Index, c)
+			}
+
+			// nextVersionIndex figures out what version of this child
+			// is present in the next parent version
+			nextVersion := nextVersionIndex(c, child, nextParent, opts)
+
+			start := 0
+			if c != nil {
+				start = c.VersionIndex + 1
+			} else {
+				// current child is not defined, is next child
+				next := child.VersionBefore(timeThresholdParent(parent, 0))
+				if next == nil {
+					start = 0
+				} else {
+					start = next.VersionIndex + 1
+				}
+			}
+
+			var updates osm.Updates
+			for k := start; k < nextVersion; k++ {
+				if child[k].Visible {
+					// it's possible for this child to be present at multiple locations in the parent
+					for _, cl := range locs {
+						u := child[k].Update()
+						u.Index = cl.Index
+						updates = append(updates, u)
+					}
+				} else {
+					// child has become not-visible between parent version
+					// this is a data inconsistency that can happen in old data
+					// i.e. pre element versioning
+					//
+					// see node 321452894, changed 7 times in the same changeset,
+					// version 5 was a delete
+					// (also node 65172196)
+					if !opts.IgnoreInconsistency {
+						return nil, fmt.Errorf("%v: %v: child deleted between parent versions", parent.ID(), fid)
+					}
+				}
+			}
+
+			// there is everything needed for this parent version
+			results[parentIndex] = append(results[parentIndex], updates...)
+		}
+	}
+
+	for _, r := range results {
+		r.SortByIndex()
+	}
+
+	return results, nil
 }
 
 // childLoc references a location of a child in the parents + children.
