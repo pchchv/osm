d7553386ce31adbcd6aab1b8fe4e70bad4517c5e
pchchv <ipchchv@gmail.com>
2025-04-10 10:15:41 +0300

    Implement an IsReverse function

--- annotate/datasource.go
+++ annotate/datasource.go
@@ -3,6 +3,8 @@ package annotate
 import (
 	"context"
 
+	"github.com/pchchv/geo"
+	"github.com/pchchv/geo/planar"
 	"github.com/pchchv/osm"
 	"github.com/pchchv/osm/annotate/internal/core"
 )
@@ -21,4 +23,25 @@ func (wds *wayChildDatasource) Get(ctx context.Context, id osm.FeatureID) (core.
 
 type wayDatasource struct {
 	NodeHistoryDatasourcer
+}
+
+// IsReverse checks if the update to this way was “reversal”.
+// This is very tricky to answer in the general case,
+// but easier for a minor update to a relation.
+// Since the relation has not been updated,
+// assume things are still connected and may just check the endpoints.
+func IsReverse(w1, w2 *osm.Way) bool {
+	if len(w1.Nodes) < 2 || len(w2.Nodes) < 2 {
+		return false
+	}
+
+	// check if either is a ring
+	if w1.Nodes[0].ID == w1.Nodes[len(w1.Nodes)-1].ID || w2.Nodes[0].ID == w2.Nodes[len(w2.Nodes)-1].ID {
+		r1 := geo.Ring(w1.LineString())
+		r2 := geo.Ring(w2.LineString())
+		return planar.Area(r1)*planar.Area(r2) < 0
+	}
+
+	// not a ring so see if endpoint were flipped
+	return w1.Nodes[0].ID == w2.Nodes[len(w2.Nodes)-1].ID && w2.Nodes[0].ID == w1.Nodes[len(w1.Nodes)-1].ID
 }
