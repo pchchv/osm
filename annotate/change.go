dab879a68d1e77ca475dbe8879aaf1c7123a1f92
pchchv <ipchchv@gmail.com>
2025-04-10 10:54:13 +0300

    Implement findPreviousNode, findPreviousWay and findPreviousRelation functions

---
+++ annotate/change.go
@@ -0,0 +1,79 @@
+package annotate
+
+import (
+	"context"
+
+	"github.com/pchchv/osm"
+)
+
+func findPreviousNode(ctx context.Context, n *osm.Node, ds osm.HistoryDatasourcer, ignoreMissing bool) (*osm.Node, error) {
+	nodes, err := ds.NodeHistory(ctx, n.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	loc, max := -1, -1
+	for i, node := range nodes {
+		if v := node.Version; v < n.Version && v > max {
+			loc, max = i, v
+		}
+	}
+
+	if loc == -1 {
+		// no version before ours
+		if ignoreMissing {
+			return nil, nil
+		}
+		return nil, &NoVisibleChildError{ID: n.FeatureID()}
+	}
+
+	return nodes[loc], nil
+}
+
+func findPreviousWay(ctx context.Context, w *osm.Way, ds osm.HistoryDatasourcer, ignoreMissing bool) (*osm.Way, error) {
+	ways, err := ds.WayHistory(ctx, w.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	loc, max := -1, -1
+	for i, way := range ways {
+		if v := way.Version; v < w.Version && v > max {
+			loc, max = i, v
+		}
+	}
+
+	if loc == -1 {
+		// no version before ours
+		if ignoreMissing {
+			return nil, nil
+		}
+		return nil, &NoVisibleChildError{ID: w.FeatureID()}
+	}
+
+	return ways[loc], nil
+}
+
+func findPreviousRelation(ctx context.Context, r *osm.Relation, ds osm.HistoryDatasourcer, ignoreMissing bool) (*osm.Relation, error) {
+	relations, err := ds.RelationHistory(ctx, r.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	loc, max := -1, -1
+	for i, relation := range relations {
+		if v := relation.Version; v < r.Version && v > max {
+			loc, max = i, v
+		}
+	}
+
+	if loc == -1 {
+		// no version before ours
+		if ignoreMissing {
+			return nil, nil
+		}
+		return nil, &NoVisibleChildError{ID: r.FeatureID()}
+	}
+
+	return relations[loc], nil
+}
