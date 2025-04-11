6e1deb055e912f2076e3eb8212f404cc89ed52b2
pchchv <ipchchv@gmail.com>
2025-04-11 12:49:20 +0300

    Implement String, Dir, Uint64 and private methods for MinuteSeqNum

--- replication/interval.go
+++ replication/interval.go
@@ -1,6 +1,9 @@
 package replication
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // State returns information about the current replication state.
 type State struct {
@@ -13,3 +16,20 @@ type State struct {
 // MinuteSeqNum indicates the sequence of the minutely diff replication found here:
 // http://planet.osm.org/replication/minute
 type MinuteSeqNum uint64
+
+// String returns 'minute/%d'.
+func (n MinuteSeqNum) String() string {
+	return fmt.Sprintf("minute/%d", n)
+}
+
+// Dir returns the directory of this data on planet osm.
+func (n MinuteSeqNum) Dir() string {
+	return "minute"
+}
+
+// Uint64 returns the seq num as a uint64 type.
+func (n MinuteSeqNum) Uint64() uint64 {
+	return uint64(n)
+}
+
+func (n MinuteSeqNum) private() {}
