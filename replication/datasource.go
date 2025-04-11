3c8f48a6367eaa66c3ccc76002288c17b31ce11b
pchchv <ipchchv@gmail.com>
2025-04-11 12:59:51 +0300

    Implement Datasource constructor

--- replication/datasource.go
+++ replication/datasource.go
@@ -1,9 +1,26 @@
 package replication
 
-import "net/http"
+import (
+	"net/http"
+	"time"
+)
+
+// DefaultDatasource is the Datasource used by the package level convenience functions.
+var DefaultDatasource = &Datasource{
+	Client: &http.Client{
+		Timeout: 30 * time.Minute,
+	},
+}
 
 // Datasource defines context around replication data requests.
 type Datasource struct {
 	BaseURL string // will use package level BaseURL if empty
 	Client  *http.Client
+}
+
+// NewDatasource creates a Datasource using the given client.
+func NewDatasource(client *http.Client) *Datasource {
+	return &Datasource{
+		Client: client,
+	}
 }
