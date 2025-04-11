a50f9d7593a4f04cd245e8097ed7013a79bb5be7
pchchv <ipchchv@gmail.com>
2025-04-11 12:45:40 +0300

    Add a State type

--- replication/interval.go
+++ replication/interval.go
@@ -1,1 +1,11 @@
 package replication
+
+import "time"
+
+// State returns information about the current replication state.
+type State struct {
+	SeqNum        uint64    `json:"seq_num"`
+	Timestamp     time.Time `json:"timestamp"`
+	TxnMax        int       `json:"txn_max,omitempty"`
+	TxnMaxQueried int       `json:"txn_max_queries,omitempty"`
+}
