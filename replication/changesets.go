7060585aaabc6a7fad5b231dba701f48bbfd5dab
pchchv <ipchchv@gmail.com>
2025-04-14 08:29:51 +0300

    Add ChangesetSeqNum type

---
+++ replication/changesets.go
@@ -0,0 +1,5 @@
+package replication
+
+// ChangesetSeqNum indicates the sequence of the changeset replication found here:
+// https://planet.osm.org/replication/changesets/
+type ChangesetSeqNum uint64
